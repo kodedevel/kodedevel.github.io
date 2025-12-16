a2730b7ffde223c7d70e753dcbe08c32ccdf280b
kodedevel <soleimani.yunes@gmail.com>
2025-12-16 07:28:58 +0330

    Update

--- resources/js/seo-config.js
+++ resources/js/seo-config.js
@@ -9,7 +9,47 @@ function applyCanonical() {
   head.insertBefore(link, head.firstElementChild);
 }
 
+function estimateTotalReadingTime() {
+  const article = document.querySelector("article");
+
+  if (!article) return;
+
+  const clone = article.cloneNode(true);
+
+  const etaForCodes = estimateSnippetReadingTime(clone);
+
+  const etaForTexts = estimateRegularTextReadingTime(clone);
+
+  const totalETA = etaForCodes + etaForTexts;
+
+  const eta = document.querySelector(".eta");
+
+  if (eta != null) eta.lastElementChild.textContent = `زمان مطالعه ${totalETA} دقیقه`;
+
+}
+
+function estimateRegularTextReadingTime(article) {
+  var numberOfWords = 0;
+  const texts = article.innerText.trim().split(/\n|\s/);
+  texts.forEach(line => {if (line.length > 0) numberOfWords++;});
+
+  return Math.max(1, Math.ceil(numberOfWords / 200));
+}
+
+function estimateSnippetReadingTime(article) {
+  var numberOfWords = 0;
+  article.querySelectorAll("pre").forEach(child => {
+    const text = child.innerText.trim().split(/\n|\s/);
+    text.forEach(line => {if (line.length > 0) numberOfWords++;});
+    //remove pre (code snippet) to make sure it won't be calculated for regular text.
+    child.remove();
+  });
+
+  return Math.max(1, Math.ceil(numberOfWords / 100));
+}
+
 function applySEOConfigurations() {
+  estimateTotalReadingTime();
   applyCanonical();
 }
 
