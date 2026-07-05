e38e22bcc82a27e2d767f4c7f19d824e24481523
kodedevel <soleimani.yunes@gmail.com>
2026-07-06 00:35:53 +0330

    Added support to handle url hashes on browser's back button clicked

--- resources/js/main.js
+++ resources/js/main.js
@@ -3,7 +3,48 @@ import {createCodeViews} from "./article.js";
 import {initUiComponents, scrollTopVisibility} from "./base.js";
 import {applySEOConfigurations} from "./seo-config.js";
 
+let isOnDocumentClicked = false;
+let hashStack = [];
+
+function clearHashStack() {
+  while (hashStack.length > 0)
+    hashStack.pop();
+}
+
+function handleBrowserBackButton() {
+
+  document.onclick = function () {
+    isOnDocumentClicked = true;
+  }
+
+  window.onhashchange = function () {
+
+    const url = new URL(window.location.href);
+
+    if (isOnDocumentClicked) {
+      isOnDocumentClicked = false;
+      hashStack.push(url.hash);
+      return;
+    } else {
+
+      isOnDocumentClicked = false;
+
+      if (hashStack.length === 0) {
+        history.back();
+      } else {
+        clearHashStack();
+        window.scrollTo({top: 0, behavior: "smooth"});
+      }
+
+    }
+
+  }
+
+}
+
 function executeCodes() {
+
+  handleBrowserBackButton();
 
   window.onscroll = scrollTopVisibility;
   createCodeViews();
