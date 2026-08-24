3a2bc9f27943e7cd7af2d05c4c2f0622e0a39251
kodedevel <soleimani.yunes@gmail.com>
2026-08-25 02:23:03 +0330

    moved eta reading functions to base

--- resources/js/seo-config.js
+++ resources/js/seo-config.js
@@ -1,3 +1,5 @@
+import {estimateRegularTextReadingTime, estimateSnippetReadingTime} from "./base.js"
+
 function estimateTotalReadingTime() {
   const article = document.querySelector("article");
 
@@ -17,35 +19,7 @@ function estimateTotalReadingTime() {
     eta.lastElementChild.textContent = `زمان مطالعه ${totalETA} دقیقه`;
 }
 
-function estimateRegularTextReadingTime(article) {
-  var numberOfWords = 0;
-  const texts = article.innerText.trim().split(/\n|\s/);
-  texts.forEach((line) => {
-    if (line.length > 0) numberOfWords++;
-  });
 
-  return Math.max(1, Math.ceil(numberOfWords / 250));
-}
-
-function estimateSnippetReadingTime(article) {
-
-  var numberOfWords = 0;
-
-  const containers = article.querySelectorAll(".snippet-container");
-
-  containers.forEach(container => {
-    const snippet = container.firstElementChild;
-    const text = snippet.innerText.split(/[\s\n]/g);
-    text.forEach(word => {
-
-      if (word.length > 0)
-        numberOfWords++;
-    });
-  });
-
-
-  return Math.ceil(numberOfWords / 100);
-}
 
 function applyOnDisplayPersianDates() {
   const datePublishedEl = document.getElementById("publish-date");
